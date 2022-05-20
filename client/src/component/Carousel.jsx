7f77c8fb931bb57166abdaf9b2ae85d38520e760
Ayan Banerjee <ayabaner@publicisgroupe.net>
2022-05-20 16:40:06 +0530

    Added slick carousel and finished homepage styling

--- client/src/component/Carousel.jsx
+++ client/src/component/Carousel.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import Slider from "react-slick";
+import PropTypes from "prop-types";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-const Carousel = () => {
-  return <div>Carousel</div>;
+const Carousel = ({ settings, children }) => {
+  return (
+    <div>
+      <h2>Pause On Hover</h2>
+      <Slider {...settings}>{children}</Slider>
+    </div>
+  );
+};
+
+Carousel.propTypes = {
+  settings: PropTypes.object.isRequired,
+  children: PropTypes.arrayOf(PropTypes.element).isRequired,
 };
 
 export default Carousel;
